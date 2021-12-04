f89c41cb461e4023fb4cb02e1a07498d149cf6e9
csae8092 <peter.andorfer@oeaw.ac.at>
2021-12-04 11:22:43 +0100

    ids and handles to data/texts/T*.xml

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "add ids"
-add-attributes -g "./app/data/letters/*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/letters"
-add-attributes -g "./app/data/indices/list*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/indices"
-add-attributes -g "./app/data/meta/E*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/meta"
-add-attributes -g "./app/data/diaries/D*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/diaries"
+# add-attributes -g "./app/data/letters/*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/letters"
+# add-attributes -g "./app/data/indices/list*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/indices"
+# add-attributes -g "./app/data/meta/E*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/meta"
+# add-attributes -g "./app/data/diaries/D*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/diaries"
+add-attributes -g "./app/data/texts/T*.xml" -b "https://id.acdh.oeaw.ac.at/schnitzler/barhschnitzler/texts"
